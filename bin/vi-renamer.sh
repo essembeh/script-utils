9582cd538fdd1e88f28961e7c31bb29f4e5b5651
Sébastien M-B <essembeh@gmail.com>
2011-10-24 15:54:34 +0200

    Update vi-renamer

--- bin/vi-renamer.sh
+++ bin/vi-renamer.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
 ## Common variables
-test -x "$EDITOR" || EDITOR=`which vi`
-AWK_BIN=`which awk`
-MV_BIN=`which mv`
-CP_BIN=`which cp`
+test -n "$EDITOR" || (EDITOR=`which vi` || exit 1)
+AWK_BIN=`which awk` || exit 1
+SED_BIN=`which sed` || exit 1
+MV_BIN=`which mv` || exit 1
+CP_BIN=`which cp` || exit 1
+MKTEMP_BIN=`which mktemp` || exit 1
+SEQ_BIN=`which seq` || exit 1
 
 ## Functions
 getLine () {
 	FILE="$1"
 	LINE="$2"
+## Head way
 	#head -n ${LINE} $FILE | tail -1
-	$AWK_BIN NR==$LINE $FILE
+## Sed way
+	$SED_BIN -n "${LINE}p" < $FILE
+# Awk way
+	#$AWK_BIN NR==$LINE $FILE
 }
 
 
 ## Main
 if [ $# -eq 0 ]; then
 	echo "Usage: $0 <FILE> <FILE> ..."
-	exit 1
+	exit 10
 fi
 
 ## Create tmp directory
-TMP=$(mktemp -d)
+TMP=`$MKTEMP_BIN -d` || exit 11
 ## Create tmp files
 OLDNAMES="$TMP/oldnames"
 NEWNAMES="$TMP/newnames"
@@ -43,7 +50,7 @@ if [ ! $COUNTA = $COUNTB ]; then
 	exit 2
 fi
 
-for I in $(seq 1 $COUNTA); do 
+for I in `$SEQ_BIN 1 $COUNTA`; do 
 	OLDNAME="$(getLine $OLDNAMES $I)"
 	NEWNAME="$(getLine $NEWNAMES $I)"
 	if [ "$OLDNAME" = "$NEWNAME" ]; then
