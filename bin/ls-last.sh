512df6de7088180a3eda70657b94181d040dfe42
Sébastien M-B <essembeh@gmail.com>
2011-10-26 23:16:15 +0200

    Update ls-last
    Now find in folder and apply grep pattern on result instead of tree expanding

--- bin/ls-last.sh
+++ bin/ls-last.sh
@@ -3,24 +3,27 @@
 ## Options
 SHOW_FULLPATH="false"
 
-# Common
+##
+## Common
+## 
 BASENAME_BIN=`which basename` || exit 1
 FIND_BIN="`which find` -mindepth 1 -maxdepth 1" || exit 1
 GREP_BIN=`which egrep` || exit 1
 CAT_BIN=`which cat` || exit 1
+SORT_BIN=`which sort` || exit 1
 
-
+##
 ## Functions
-__expandPath () {
-	pathToExpand=$1
-	listOfFiles=`eval echo $pathToExpand`
-	for currentFile in $listOfFiles; do 
-		test -f "$currentFile" && echo "$currentFile"
-	done
+##
+__findFiles () {
+	folder="$1"
+	pattern="$2"
+	find "$folder" -type f 2>> /dev/null | $SORT_BIN | $GREP_BIN "$pattern" 
 }
 
-
+##
 ## Retrieve CONF
+##
 appBinary="$0"
 while [ -L "$appBinary" ]; do
 	appLink="$(readlink "$appBinary")"
@@ -31,15 +34,16 @@ while [ -L "$appBinary" ]; do
 	fi  
 done
 appConfigurationFile="${appBinary%.sh}.conf"
-
 if [ ! -f "$appConfigurationFile" ]; then
 	echo "Cannot find configuration file: $appConfigurationFile"
-	exit 1
+	exit 2
 fi
 
+##
 ## Main
-$GREP_BIN -v "^#" "$appConfigurationFile" | while read currentPath; do
-	lastFile=`__expandPath "$currentPath" | tail -1`
+##
+$GREP_BIN -v "^#" "$appConfigurationFile" | while read currentPath currentPattern; do
+	lastFile=`__findFiles "$currentPath" "$currentPattern" | tail -1`
 	if [ "$SHOW_FULLPATH" = "true" ]; then
 		echo "$lastFile"
 	else
