91891e3dd33c89d134176693c025f7614874915b
Sébastien M-B <essembeh@gmail.com>
2011-10-28 20:13:15 +0200

    Adding options to srt-check.sh

--- bin/srt-check.sh
+++ bin/srt-check.sh
@@ -13,38 +13,51 @@ MOVIE_EXTENSIONS="avi mkv mpg mpeg mp4 mov"
 SRT_EXTENSIONS="srt"
 
 ##
+## Options
+##
+SHOW_ERROR="true"
+SHOW_MISSING="true"
+SHOW_ORPHAN="true"
+
+##
 ## Process file
 ##
 __processFile () {
 	theFile="$1"
-	for currentExtension in $MOVIE_EXTENSIONS; do 
-		if echo "$theFile" | $GREP_BIN -q "$currentExtension$"; then
-			if __findSRT "$theFile"; then
-				return 0
-			else
-				echo "m   Missing subtitle for movie: $theFile"
-				return 1
+	if [ "$SHOW_ORPHAN" = "true" ]; then
+		for currentExtension in $MOVIE_EXTENSIONS; do 
+			if echo "$theFile" | $GREP_BIN -q "$currentExtension$"; then
+				if __findMissing "$theFile"; then
+					return 0
+				else
+					echo "orphan  $theFile"
+					return 1
+				fi
 			fi
-		fi
-	done
-	for currentExtension in $SRT_EXTENSIONS; do 
-		if echo "$theFile" | $GREP_BIN -q "$currentExtension$"; then
-			if __findMovie "$theFile"; then
-				return 0
-			else
-				echo "o   Orphan subtitle: $theFile"
-				return 1
+		done
+	fi
+	if [ "$SHOW_MISSING" = "true" ]; then
+		for currentExtension in $SRT_EXTENSIONS; do 
+			if echo "$theFile" | $GREP_BIN -q "$currentExtension$"; then
+				if __findOrphan "$theFile"; then
+					return 0
+				else
+					echo "missing $theFile"
+					return 1
+				fi
 			fi
-		fi
-	done
-	echo "e   Unknwon file: $theFile"
+		done
+	fi
+	if [ "$SHOW_ERROR" = "true" ]; then
+		echo "error   $theFile"
+	fi
 	return 2
 }
 
 ##
 ## Find SRT for given file
 ##
-__findSRT () {
+__findMissing () {
 	movieFile="$1"
 	for currentExtension in $SRT_EXTENSIONS; do 
 		srtFile=`echo "$movieFile" | $SED_BIN -r "s/[[:alnum:]]+$/$currentExtension/"`
@@ -56,7 +69,7 @@ __findSRT () {
 ##
 ## Find movie for given srt
 ##
-__findMovie () {
+__findOrphan () {
 	srtFile="$1"
 	for currentExtension in $MOVIE_EXTENSIONS; do 
 		movieFile=`echo "$srtFile" | $SED_BIN -r "s/[[:alnum:]]+$/$currentExtension/"`
@@ -87,5 +100,22 @@ __main () {
 		fi
 	done
 }
+
+endOfLoop="false"
+while [ "$endOfLoop" = "false" ]; do 
+	case "$1" in 
+		"--no-orphan")
+			SHOW_ORPHAN="false"
+			shift;;
+		"--no-missing")
+			SHOW_MISSING="false"
+			shift;;
+		"--no-error")
+			SHOW_ERROR="false"
+			shift;;
+		*)
+			endOfLoop="true";;
+	esac
+done
 
 __main "$@"
