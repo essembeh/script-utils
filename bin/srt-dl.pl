0351b3b0cbd75d46124b87a124cbfd089f34219c
Sébastien M-B <essembeh@gmail.com>
2011-11-06 22:55:22 +0100

    Refactoring
    Code refactoring to simplify using other source website.

--- bin/srt-dl.pl
+++ bin/srt-dl.pl
@@ -4,7 +4,8 @@
 ##
 ##
 ## Dependencies (for Debian GNU/Linux): 
-##   apt-get install libwww-perl libtext-levenshtein-perl
+##   apt-get install libwww-perl libtext-levenshtein-perl libarchive-any-perl
+##
 ## Using cpan:
 ##   install Text::Levenshtein 
 ##
@@ -13,16 +14,17 @@ package org::essembeh::script;
 use strict;
 use LWP::Simple;
 use File::Basename;
-use Archive::Zip;
+use Archive::Extract;
 use Getopt::Long;
-use Text::Levenshtein qw(distance);
-use File::Temp qw/:POSIX/;
+use Text::Levenshtein qw/distance/;
+use File::Temp qw/tempfile tempdir/;
 use File::Copy;
 
 
-our (%OPTIONS, $STEU_PREFIX, $LEVENSHSTEIN_OPTIM);
+our ($VERBOSE, $STEU_PREFIX, $LEVENSHSTEIN_OPTIM);
+
 $STEU_PREFIX = "http://www.sous-titres.eu/series/";
-$LEVENSHSTEIN_OPTIM = "fr.vf";
+$LEVENSHSTEIN_OPTIM = ".fr.vf";
 
 
 ##
@@ -32,12 +34,33 @@ sub printMessage {
 	my $type = $_[0];
 	unless ($type) {
 		print "\n";
-	} elsif ($OPTIONS{"verbose"} || $type ne "debug") {
+	} elsif ($VERBOSE || $type ne "debug") {
 		for (my $i = 1; $i < @_; $i++) {
 			$type = "[".$type."]";
 			printf("%10s %s\n", $type, $_[$i]);
 		}
 	}
+}
+
+##
+##
+##
+sub findSrtInFolder {
+	my $folder = $_[0];
+	opendir (my $dh, $folder);
+	my @content;
+	foreach my $file (readdir $dh) {
+		unless ($file =~ /^\.{1,2}$/) {
+			$file = $folder."/".$file;
+			if (-d $file) {
+				push (@content, &findSrtInFolder($file));
+			} elsif (-f $file && $file =~ /\.srt$/) {
+				push (@content, $file);
+			}
+		}
+	}
+	&printMessage("debug", "Found ".@content." file(s) in folder: ".$folder);
+	@content;
 }
 
 ##
@@ -55,7 +78,7 @@ sub unifString {
 	my $str = $_[0];
 	$str =~ tr/[A-Z]/[a-z]/;
 	$str =~ s/[^A-Za-z0-9]/./g;
-	$str =~ s/[sS](?:0*)([0-9]+)[eE]([0-9]+)/$1$2/;
+	$str =~ s/[sS]0*([0-9]+)[eE]([0-9]+)/$1$2/;
 	$str =~ s/([0-9]+)x([0-9]+)/$1$2/;
 	my @words = sort(split(/\./, $str));
 	$str = "";
@@ -78,23 +101,13 @@ sub myDistance {
 }
 
 ##
-## Get the serie identifier on soust-titres.eu
-##
-sub getSerieIdentifier {
-	my $serieName=$_[0];
-	my $serieId = $serieName;
-	$serieId =~ tr/[A-Z]/[a-z]_/; 
-	$serieId =~ tr/ ./_/;
-	&printMessage("debug", "Serie identifier: : ".$serieId);
-	$serieId;
-}
-
-##
 ## Get the serie URL on STEU
 ##
-sub getSerieHomepage {
+sub getSTEUSerieHomepage {
 	my $serieName = $_[0];
-	my $url = $STEU_PREFIX.&getSerieIdentifier($serieName).".html";
+	$serieName =~ tr/[A-Z]/[a-z]_/; 
+	$serieName =~ s/[\. ]/_/g;
+	my $url = $STEU_PREFIX.$serieName.".html";
 	&printMessage("debug", "Serie Homepage: ".$url);
 	$url;
 }
@@ -102,84 +115,128 @@ sub getSerieHomepage {
 ##
 ## Get Episode number
 ##
-sub getEpisodeNumber {
-	my $episode = $_[0];
-	my $number = $episode;
-	if ($episode =~ /.*[sS](\d+)[eE](\d+).*/) {
-		$number = $1."x".$2;
-		$number =~ s/^0//;
-		&printMessage("debug", "Episode number: ".$number);
-		$number;
-	} else {
-		undef;
+sub getInformationFromEpisodeName {
+	my $file = $_[0];
+	my %infos;
+	if ($file =~ /(.*)[ .][sS]0*(\d+)[eE](\d+).*/) {
+		$infos{serie} = $1;
+		$infos{season} = $2;
+		$infos{episode} = $3;
+		&printMessage("debug", "Serie name: ".$infos{serie}.", season: ".$infos{season}.", ep: ".$infos{episode});
+		%infos;
 	}
-}
-
-##
-## Retreives the serie name from a filename
-##
-sub getSerieNameFromFile {
-	my $name = &myBasename($_[0]);
-	$name =~ s@[\. ][sS]\d+[eE]\d+.*@@;
-	&printMessage("debug", "Serie name: ".$name);
-	$name;
 }
 
 ##
 ## Get all zip corresponding the
 ##
-sub getZipFileInPage {
-	my $url=$_[0];
-	my $filter = $_[1];
-	my @zipFiles;
-	my $html = get($url);
+sub getAllPackForSerie {
+	my %fileInfos=@_;
+	my $serieHomepage = &getSTEUSerieHomepage($fileInfos{serie});
+	my %listOfPacks;
+	my $html = get($serieHomepage);
 	if ($html) {
 		my @html = split("\n", $html);
 		foreach my $line (@html) {
 			if ($line =~ /href=.(.*\.zip)/) {
-				my $zipUrl=$1;
-				if ($OPTIONS{"allzip"} || $zipUrl =~ /$filter/) {
-					push(@zipFiles, $zipUrl);
-				}
+				my $value=$STEU_PREFIX.$1;
+				my $key=&myBasename($value);
+				&printMessage("debug", "Found pack: ".$key);
+				$listOfPacks{$key} = $value;
 			}
 		}
-	}
-	if (@zipFiles > 0) {
-		&selectInList(@zipFiles);
+		%listOfPacks;
 	}
 }
 
 ##
 ##
 ##
-sub selectInList {
-	my $max = @_;
-	if ($max > 1) {
-		@_ = sort { &myBasename($a) cmp &myBasename($b) } @_;
-		for (my $i = 0; $i < $max; $i++) {
-			my $file = &myBasename($_[$i]);
-			&printMessage("question", "[".$i."] ".$file);
+sub autoChoosePack {
+	my ($season, $episode, %listOfPacks) = @_;
+	my $pattern = sprintf("%dx%02d", $season, $episode);
+	my $selectedPack; 
+	while ( my ($key, $value) = each(%listOfPacks) ) {
+		if ($key =~ /$pattern/) {
+			&printMessage("debug", "Auto choose pack: ".$key);
+			$selectedPack = $value;
+			last
 		}
-		my $item;
-		do {
-			&printMessage("question", "Select a file: [0-".($max-1)."]?");
-			chop($item = <STDIN>);
-		}until ($item =~ /^\d+$/ && $item>=0 && $item < $max);
-		$_[$item];
-	} else {
-		$_[0];
 	}
+	$selectedPack;
 }
 
 ##
 ##
 ##
-sub downloadFileInTmp {
+sub getAllSubtitlesForPack {
+	my $packUrl = $_[0];
+	# Download the pack
+	my $tmpFile = &downloadTmpFile($packUrl);
+	# Unzip the pack
+	my $zipObject = Archive::Extract->new(archive => $tmpFile, type => 'zip') or die "Error openning zip:".$tmpFile;
+	my $tmpDir = tempdir();
+	&printMessage("debug", "Extract zip: ".$tmpFile.", to folder:".$tmpDir);
+	$zipObject->extract(to => $tmpDir);
+	my @content = &findSrtInFolder($tmpDir);
+	my %content;
+	foreach my $srtFile (@content) {
+		my $basename = &myBasename($srtFile);
+		$content{$basename} = $srtFile;
+	}
+	%content;
+}
+
+##
+##
+##
+sub autoChooseSubtitle {
+	my $reference = shift;
+	my %listOfSubtitles = @_;
+	my $bestDistance = -1;
+	my $bestKey;
+	my $bestValue;
+	while ( my ($key, $value) = each(%listOfSubtitles) ) {
+		my $distance = &myDistance($reference.$LEVENSHSTEIN_OPTIM, $key);
+		if ($bestDistance < 0 || $distance < $bestDistance) {
+			$bestKey = $key;
+			$bestValue = $value;
+			$bestDistance = $distance;
+		}
+	}
+	&printMessage("debug", "Auto choose srt: ".$bestKey);
+	$bestValue;
+}
+
+##
+##
+##
+sub userSelection {
+	my %hash = @_;
+	my @keys = sort(keys %hash);
+	my $max = @keys;
+	for (my $i = 0; $i < $max; $i++) {
+		my $key = @keys[$i];
+		&printMessage("question", "[".$i."] ".$key);
+	}
+	my $item;
+	do {
+		&printMessage("question", "Select a file: [0-".($max-1)."]?");
+		chop($item = <STDIN>);
+	}until ($item =~ /^\d+$/ && $item>=0 && $item < $max);
+	my $selection = $hash{$keys[$item]};
+	&printMessage("debug", "Selection: ".$selection);
+	$selection;
+}
+
+##
+##
+##
+sub downloadTmpFile {
 	my $url = $_[0];
 	my $document = get($url);
 	if ($document) {
-		my $tmpPath = tmpnam();
-		my $tmpFile = FileHandle->new($tmpPath, "w");
+		my ($tmpFile, $tmpPath) = tempfile();
 		$tmpFile->write($document);
 		$tmpFile->close();
 		&printMessage("debug", "File successfully downloaded: ".$url." --> ".$tmpPath);
@@ -189,42 +246,6 @@ sub downloadFileInTmp {
 	}
 }
 	
-##
-##
-##
-sub extractSrtFromZip {
-	 my $zipPath = $_[0];
-	 my $target = $_[1];
-	 my $zipObject = Archive::Zip->new($zipPath) or die "Error openning zip:".$zipPath;
-	 my @content;
-	 my $betterFile;
-	 my $betterDistance = 0;
-	 my $targetOptim = $target;
-	 &printMessage("debug", "Opening zip: ".$zipPath);
-	 $targetOptim =~ s/srt$/$LEVENSHSTEIN_OPTIM.srt/;
-	 &printMessage("debug", "Target for distance computing: ".$targetOptim);
-	 foreach my $member ($zipObject->members) {
-		 my $filename = $member->fileName();
-		 if ($filename =~ /srt$/) {
-			my $distance = &myDistance($filename, $targetOptim);
-			unless ($OPTIONS{"allsrt"}) {
-				if (!$betterFile || abs($distance) < $betterDistance) {
-					$betterFile = $filename;
-					$betterDistance = abs($distance);
-				}
-			}
-			push(@content, $filename);
-		 }
-	 }
-	 if ($OPTIONS{"allsrt"}) {
-		 $betterFile = &selectInList(@content);
-	 } else {
-		 &printMessage("debug", "Auto choosing file: ".$betterFile." with distance: ".$betterDistance);
-	 }
-	 $zipObject->extractMember($betterFile, $target);
-	 &printMessage("info", "File copied: ".$betterFile);
-}
-
 ##
 ##
 ##
@@ -239,9 +260,9 @@ sub computeTargetSrtFile {
 ##
 ## Removes tags from srt
 ##
-sub removeTag {
+sub removeTagFromSubtitle {
 	my $srtPath = $_[0];
-	my $tmpPath = tmpnam();
+	my ($tmpFile, $tmpPath) = tempfile();
 	&printMessage("debug", "Backup srt file: ".$tmpPath);
 	copy ($srtPath, $tmpPath);
 	my $srtFile = FileHandle->new($srtPath, "w");
@@ -252,18 +273,29 @@ sub removeTag {
 		$line =~ s/\{[^\}].*\}//g and $nbTags++;
 		$srtFile->write($line);
 	}
-	&printMessage("debug", "Tags removed: ".$nbTags);
+	&printMessage("info", "Clean subtitle: ".$nbTags." tag(s) removed");
 	$tmpFile->close();
 	$srtFile->close();
 }
 
+##
+##
+##
+sub copySubtitle () {
+	my $source = $_[0];
+	my $destination = $_[1];
+	&printMessage("info", "Copying: ".&myBasename($source));
+	copy($source, $destination);
+}
+	
 
 ##
 ##
 ##
 sub displayHelp {
-    print"NAME
-    srt-dl - Sub title downloader version 1.0
+    print"\
+NAME
+    srt-dl - Sub title downloader version 1.1
 
 USAGE
     srt-dl [OPTIONS] <EPISODE> <EPISODE> ...
@@ -281,14 +313,14 @@ OPTIONS
     --noverbose (default)
         To display debug informations.
 
-    --allzip 
-    --noallzip (default)
-        Automatically try to fetch the zip corresponding to the 
+    --autopack (default)
+    --noautopack 
+        Automatically try to fetch the pack of subtitles corresponding to the 
         given episode from its number.
         If no result, try --allzip to show all available srt zip files.
     
-    --allsrt 
-    --noallsrt (default)
+    --autosrt (default)
+    --noautosrt
         Automatically try to choose the srt that better matches the episode 
         name using the Levenshtein distance.
 
@@ -298,8 +330,8 @@ OPTIONS
 
 EXAMPLES
     srt-dl --force --verbose --clean My.Serie.S02E04.mkv
-        Automatically try to dl zip for the episode 2x04.
-        Then choose the better SRT. 
+        Automatically try to download subtitles for the episode 2x04.
+        Then choose the better one. 
         Finally, the srt file will get its tags removed.
         Note: if a My.Serie.S02E04.srt already exists, it will be overwritten.
 
@@ -312,61 +344,89 @@ COPYRIGHT
 ##
 ## Main 
 ##
-
-unless (GetOptions(\%OPTIONS, "help", "verbose", "force", "allzip", "allsrt", "clean")) {
+my ($optionAutopack, $optionAutosrt, $optionForce, $optionClean, $optionHelp) = (1, 1, 0, 0, 0);
+unless (GetOptions(	"help"       => \$optionHelp, 
+					"v|verbose!" => \$VERBOSE, 
+					"f|force!"   => \$optionForce, 
+					"autosrt!"   => \$optionAutosrt, 
+					"autopack!"  => \$optionAutopack, 
+					"c|clean!"   => \$optionClean)) {
 	displayHelp;
 	exit 1;
 }
 
-if ($OPTIONS{"help"}) {
+if ($optionHelp) {
 	displayHelp;
 	exit 0;
 }
 
-my $firstLoop = 1;
-foreach my $episode (@ARGV) {
-	if ($firstLoop) {
-		$firstLoop = 0;
-	} else {
-		&printMessage();
-	}
-	&printMessage("info", "Fetching srt for episode: ".$episode);
-	my $target=&computeTargetSrtFile($episode);
-	if (-f $target) {
-		if ($OPTIONS{"force"}) {
-			&printMessage("debug", "Srt already exists, will be overwritten");
+my $count = 0;
+foreach my $file (@ARGV) {
+	&printMessage() if ($count ++);
+	&printMessage("info", "Fetching srt for episode: ".$file);
+	
+	## Step 0: Compute srt target file
+	my $targetSrtFile=&computeTargetSrtFile($file);
+	if (-f $targetSrtFile) {
+		if ($optionForce) {
+			&printMessage("info", "Srt already exists, will be overwritten");
 		} else {
-			&printMessage("info", "The srt file already exists");
+			&printMessage("error", "The srt file already exists (use --force to overwrite)");
 			next;
 		}
 	}
-	my $serie = &getSerieNameFromFile($episode);
-	unless ($serie) {
-		&printsage("error", "Error getting the serie name");
+
+	## Step 1: File information
+	my %fileInfos = &getInformationFromEpisodeName($file);
+	unless (%fileInfos) {
+		&printMessage("error", "Error getting informations from filename: ".$file);
 		next;
 	}
-	my $url = &getSerieHomepage($serie);
-	unless ($url) {
-		&printMessage("error", "Error getting serie homepage");
+	
+	## Step 2a: Get all zip
+	my %listOfPacks = &getAllPackForSerie(%fileInfos);
+	unless (%listOfPacks) {
+		&printMessage ("error", "Cannot find any pack for the serie");
 		next;
 	}
-	my $episodeNumber = &getEpisodeNumber($episode);
-	unless ($episodeNumber) {
-		&printMessage("warning", "Cannot retrieve episode number");
+
+	## Step 2b: Choose a pack
+	my $selectedPackUrl;
+	if ($optionAutopack) {
+		$selectedPackUrl = &autoChoosePack ($fileInfos{season}, $fileInfos{episode}, %listOfPacks);
+	} else {
+		$selectedPackUrl = &userSelection (%listOfPacks);
 	}
-	my $zipUrl = &getZipFileInPage($url, $episodeNumber);
-	unless ($zipUrl) {
-		&printMessage("error", "No zip file found");
+	unless ($selectedPackUrl) {
+		&printMessage("error", "Invalid pack");
 		next;
 	}
-	my $zipFile = &downloadFileInTmp($STEU_PREFIX.$zipUrl);
-	unless ($zipFile) {
-		&printMessage("error", "Error downloading zip file: ".$zipUrl);
+
+	## Step 3a: Get srt list
+	my %listOfSubtitles = &getAllSubtitlesForPack($selectedPackUrl);
+	unless (%listOfSubtitles) {
+		&printMessage("error", "No srt found selected pack");
 		next;
 	}
-	&extractSrtFromZip($zipFile, $target);
-	if ($OPTIONS{"clean"}) {
-		&removeTag($target);
+		
+	## Step 3b: Choose a srt
+	my $selectedSubtitleUrl;
+	if ($optionAutosrt) {
+		$selectedSubtitleUrl = &autoChooseSubtitle ($targetSrtFile, %listOfSubtitles);
+	} else {
+		$selectedSubtitleUrl = &userSelection (%listOfSubtitles);
+	}
+	unless ($selectedSubtitleUrl) {
+		&printMessage("error", "Invalid subtitle");
+		next;
+	}
+
+	## Step 4: Copy the srt
+	&copySubtitle($selectedSubtitleUrl, $targetSrtFile);
+
+	## Step 5: Clean the srt
+	if ($optionClean) {
+		&removeTagFromSubtitle($targetSrtFile);
 	}
 }
 
