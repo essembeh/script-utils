30bb61924425fc50bbbea9ed4e0f0fe30150582e
Sébastien M-B <essembeh@gmail.com>
2012-06-13 10:35:22 +0200

    Better passme algo

--- bin/passme.sh
+++ bin/passme.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+function getMoreHash {
+	 echo "$1" | sha1sum | awk '{print $1}' 
+}
+
 PASSMESIZE=16
 PASSMEFILE=~/.passme.secret
 if test -f "$PASSMEFILE"; then
-	PASSMASTER=`head -1 "$PASSMEFILE"`
+	MASTER=`head -1 "$PASSMEFILE"`
 else
 	echo "Enter master password?"
-	read PASSMASTER
+	read MASTER
 fi
-
+HMASTER=`getMoreHash "$MASTER"`
 while [[ $# > 0 ]]; do
 	SEED="$1"; shift
-	echo -n "Computing pasword for $SEED --> "
-	echo "$PASSMASTER$SEED" | sha1sum | awk '{print $1}' | base64 | head -c $PASSMESIZE
+	HSEED=`getMoreHash "$SEED"`
+	HKEY=`getMoreHash "$HSEED$HMASTER"`
+	echo -n "$SEED --> "
+	echo "$HKEY" | base64 | head -c $PASSMESIZE
 	echo 
 done
 
