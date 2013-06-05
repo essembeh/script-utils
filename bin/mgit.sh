3949e51e8ecf8e65000a598d3a9b70535705c62f
Sébastien M-B <essembeh@gmail.com>
2013-06-05 12:32:41 +0200

    Better mgit

--- bin/mgit.sh
+++ bin/mgit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-GIT=/usr/bin/git
+CONF="./mgit.conf"
+GIT="/usr/bin/git"
 
 NORMAL="\e[0;39m" 
 VERT="\e[1;32m" 
@@ -12,21 +13,40 @@ BLANCLAIR="\e[1;08m"
 JAUNE="\e[1;33m" 
 CYAN="\e[1;36m"
 
+function main () {
+	if test -f "$CONF"; then
+		echo "Using $CONF"
+		LIST=`cat "$CONF"`
+	else
+		LIST=`find -L . -name ".git" -type d | sort`
+	fi 
+	for GIT_FOLDER in $LIST; do 
+		if test -d "$GIT_FOLDER"; then
+			PROJECT_FOLDER=`dirname "$GIT_FOLDER"`
+			PROJECT_NAME=`basename "$PROJECT_FOLDER"`
+			BRANCH=`$GIT --git-dir="$GIT_FOLDER" --work-tree="$PROJECT_FOLDER" rev-parse --abbrev-ref HEAD`
+			STATUS=`$GIT --git-dir="$GIT_FOLDER" --work-tree="$PROJECT_FOLDER" status --porcelain`
+			if test -z "$STATUS"; then 
+				BRANCH_COLOR=$VERT
+			else 
+				BRANCH_COLOR=$ROUGE
+			fi
+			
 
-LIST=`find . -name ".git" -type d | sort`
-for GIT_FOLDER in $LIST; do 
-	if test -d "$GIT_FOLDER"; then
-		PROJECT_FOLDER=`dirname "$GIT_FOLDER"`
-		PROJECT_NAME=`basename "$PROJECT_FOLDER"`
-		BRANCH=`$GIT --git-dir="$GIT_FOLDER" --work-tree="$PROJECT_FOLDER" rev-parse --abbrev-ref HEAD`
-
-		printf "$ROUGE%s$NORMAL  $VERT%s$NORMAL \n" "$PROJECT_NAME" "($BRANCH)" 
-		if test $# -gt 0; then 
-			echo "      _________________________________________________________________"
-			$GIT --git-dir="$GIT_FOLDER" --work-tree="$PROJECT_FOLDER" $@ 2>&1 | sed "s/^/     |  /" 
-			echo "     |_________________________________________________________________"
+			echo -e "========================================================================================================="
+			printf "$JAUNE>>>  $CYAN%s$NORMAL  $BRANCH_COLOR%s$NORMAL \n" "$PROJECT_NAME" "($BRANCH)" 
+			if test $# -gt 0; then 
+				echo ""
+				$GIT --git-dir="$GIT_FOLDER" --work-tree="$PROJECT_FOLDER" $@ 
+			fi
+			echo -e "_________________________________________________________________________________________________________"
 		fi
-		echo ""
-	fi
-done
+	done
+}
 
+if [ "$1" = "-l" ]; then
+	shift
+	main $@ 2>&1 | less 
+else
+	main $@
+fi
