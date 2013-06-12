f7426940d91133d3d757232db0a9e44bfd8e7ffe
Sébastien M-B <essembeh@gmail.com>
2013-06-12 14:35:01 +0200

    Update mgit
    Adding better line filling char

--- bin/mgit.sh
+++ bin/mgit.sh
@@ -6,42 +6,61 @@ MGIT_LIST="conf"
 
 GIT="/usr/bin/git"
 
-NORMAL="\e[0;39m" 
-VERT="\e[1;32m" 
-ROUGE="\e[1;31m" 
-ROSE="\e[1;35m" 
-BLEU="\e[1;34m"
-BLANC="\e[0;02m" 
-BLANCLAIR="\e[1;08m" 
-JAUNE="\e[1;33m" 
+NORMAL="\e[0m" 
+GREEN="\e[1;32m" 
+RED="\e[1;31m" 
+PINK="\e[1;35m" 
+BLUE="\e[1;34m"
+WHITE="\e[0;02m" 
+YELLOW="\e[1;33m" 
 CYAN="\e[1;36m"
+
+function fillLine() {
+	ITEM="$1"
+	if test -z "$ITEM"; then
+		echo ""
+	else
+		WIDTH=`tput cols` 
+		ITEMLEN=`printf "$ITEM" | wc -m`
+		if test $ITEMLEN -gt $WIDTH; then 
+			printf "$ITEM\n"
+		else 
+			COUNT=$(($WIDTH / $ITEMLEN))
+			for I in `seq 1 $COUNT`; do
+				printf "$ITEM"
+			done
+			printf "\n"
+		fi
+	fi
+}
 
 function main () {
 	if test "$MGIT_LIST" = "conf" && test -f "$MGIT_CONF"; then
 		LIST=`cat "$MGIT_CONF"`
 	else
-		LIST=`find -L . -name ".git" -type d | sort`
+		LIST=`find -L . -name ".git" -type d -exec dirname {} \; | sort`
 	fi 
-	for GIT_FOLDER in $LIST; do 
-		if test -d "$GIT_FOLDER"; then
-			PROJECT_FOLDER=`dirname "$GIT_FOLDER"`
-			PROJECT_NAME=`basename "$PROJECT_FOLDER"`
-			BRANCH=`$GIT --git-dir="$GIT_FOLDER" --work-tree="$PROJECT_FOLDER" rev-parse --abbrev-ref HEAD`
-			STATUS=`$GIT --git-dir="$GIT_FOLDER" --work-tree="$PROJECT_FOLDER" status --porcelain`
-			if test -z "$STATUS"; then 
-				BRANCH_COLOR=$VERT
-			else 
-				BRANCH_COLOR=$ROUGE
-			fi
-			
+	for PROJECT_FOLDER in $LIST; do 
+		if test -d "$PROJECT_FOLDER"; then
+			(cd "$PROJECT_FOLDER"
+				PROJECT_NAME=`basename "$PROJECT_FOLDER"`
+				BRANCH=`$GIT rev-parse --abbrev-ref HEAD`
+				STATUS=`$GIT status --porcelain`
+				if test -z "$STATUS"; then 
+					BRANCH_COLOR=$GREEN
+				else 
+					BRANCH_COLOR=$RED
+				fi
+				
 
-			echo -e "========================================================================================================="
-			printf "$JAUNE>>>  $CYAN%s$NORMAL  $BRANCH_COLOR%s$NORMAL \n" "$PROJECT_NAME" "($BRANCH)" 
-			if test $# -gt 0; then 
-				echo ""
-				$GIT --git-dir="$GIT_FOLDER" --work-tree="$PROJECT_FOLDER" $@ 
-			fi
-			echo -e "_________________________________________________________________________________________________________"
+				fillLine "="
+				printf "$YELLOW>>>  $CYAN%s$NORMAL  $BRANCH_COLOR%s$NORMAL \n" "$PROJECT_NAME" "($BRANCH)" 
+				if test $# -gt 0; then 
+					echo ""
+					$GIT $@ 
+				fi
+				fillLine "_"
+			)
 		fi
 	done
 }
