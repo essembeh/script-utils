e1e6229adc7170681ad2854fe4f25ac732d0e00d
Sébastien M-B <seb@essembeh.org>
2014-06-05 09:06:11 +0200

    Update auto-rdb
    Path can be relative to conf file if begins with ./

--- bin/auto-rdb.sh
+++ bin/auto-rdb.sh
@@ -19,15 +19,19 @@ else
 fi
 
 cat "$CONF_FILE" | grep "^[^#]" | while read SOURCE TARGET_DIR OPTIONS; do 
-	if test -z "$SOURCE" -o ! -d "$SOURCE"; then
-		echo "Problem with source folder: $SOURCE"
+	if test -z "$SOURCE" -o -z "$TARGET_DIR"; then
+		echo "Problem with configuration file"
 		continue
 	fi
-	if test -z "$TARGET_DIR"; then
-		echo "Problem with destination folder: $TARGET_DIR"
+	if test "${SOURCE:0:2}" = "./"; then
+		SOURCE="$(dirname "$CONF_FILE")/${SOURCE:2}"
 	fi
 	SOURCE_BASENAME=$(basename "$SOURCE")
 	test -n "$SOURCE_BASENAME"
+	if ! test -d "$SOURCE"; then
+		echo "Cannot find source folder: $SOURCE"
+		continue
+	fi
 	TARGET="$TARGET_DIR/$SOURCE_BASENAME$FOLDER_EXT"
 	echo "Backup: $SOURCE --> $TARGET ($OPTIONS)"
 	rdiff-backup $OPTIONS "$SOURCE" "$TARGET"
