026fda8283759127fe93907644277f0a8bc9a4be
Sébastien MB <seb@essembeh.org>
2018-07-18 00:03:53 +0200

    Update borg-save to add custom args

--- borg-save.sh
+++ borg-save.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 set -e
 
-BORG_ENV=".borg.env"
-BORG_ARGS="--stats --progress"
+BORG_ENV_FILE=".borg.env"
+BORG_ARGS_FILE=".borg.args"
+BORG_DEFAULT_ARGS="--stats --progress"
 
 for FOLDER in "$@"; do
 	echo "=============================================================================="
 	test -d "$FOLDER"
 	(
 		cd "$FOLDER"
-		NAME=`basename "$PWD"`
+		NAME=$(basename "$PWD")
 		echo "Backup $PWD"
 
-		if test -r "$BORG_ENV"; then
-			echo "Sourcing $PWD/$BORG_ENV"
-			source "$BORG_ENV"
+		if test -r "$BORG_ENV_FILE"; then
+			echo "Sourcing $PWD/$BORG_ENV_FILE"
+			source "$BORG_ENV_FILE"
+		else
+			echo "Cannot find $PWD/$BORG_ENV_FILE"
 		fi
 
 		if test -z "$BORG_REPO"; then
 			echo "BORG_REPO is not set"
 			exit 1
+		fi
+
+		BORG_ARGS="$BORG_DEFAULT_ARGS"
+		if test -r "$BORG_ARGS_FILE"; then
+			BORG_ARGS=$(cat "$BORG_ARGS_FILE")
+			echo "Found $PWD/$BORG_ARGS_FILE, using args: $BORG_ARGS"
+		else
+			echo "Cannot find $PWD/$BORG_ARGS_FILE, using defaults args: $BORG_ARGS"
 		fi
 
 		echo ""
@@ -28,6 +39,7 @@ for FOLDER in "$@"; do
 		
 		cd ..
 		test -d "$NAME"
+		pwd
 		echo ""
 		echo -n "Backup $PWD/$NAME to $BORG_REPO ::${NAME}@{now} ? (Y/n)"
 		read YN
