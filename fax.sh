19ab2c2e30562f8ee1d4392a6edb7ad1a7a2f174
Sébastien MB <seb@essembeh.org>
2019-11-10 00:08:37 +0100

    Update fax

--- fax.sh
+++ fax.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
-set -e
+set -eu
 
-for FILE in "$@"; do 
-	test -f "$FILE"
-	OUTPUT="${FILE%.*}"
-	test -d "$OUTPUT" && OUTPUT="${OUTPUT}.$RANDOM"
-	7z x -o"$OUTPUT" "$FILE"
-	rm -iv "$FILE"
+for FILE in "$@"; do
+	echo "----------[$FILE]----------"
+	if test -f "$FILE"; then
+		# Create a uniq target folder for extraction
+		TARGET=${FILE%.*}
+		if test -e "$TARGET"; then
+			TARGET="$TARGET.$RANDOM"
+		fi
+		mkdir -v "$TARGET"
+		# Extract file using tar or 7z
+		case "$(basename "$FILE")" in
+			*.tar|*.tar.*|*.tgz)
+				tar -C "$TARGET" -xavf "$FILE"
+				;;
+			*)
+				7z x -o"$TARGET" "$FILE"
+				;;
+		esac
+		# Check if archive only contains one folder/file
+		FILES=$(ls -A1 "$TARGET" 2>/dev/null)
+		if test $(echo "$FILES" | wc -l) -eq 1; then
+			NEWTARGET="$(dirname "$FILE")/$FILES"
+			if ! test -e "$NEWTARGET"; then
+				mv -nv "$TARGET/$FILES" "$NEWTARGET"
+				rmdir -v "$TARGET"
+			fi
+		fi
+	fi
+	echo ""
 done
 
