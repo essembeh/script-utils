b3d47fa1c3c7b1ab72b5dd028b7af62bf477aece
Sébastien MB <seb@essembeh.org>
2022-02-17 18:34:47 +0100

    Add multi thread for hrenamer

--- script_utils/hrenamer.py
+++ script_utils/hrenamer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import hashlib
 import shutil
 import sys
@@ -57,23 +58,26 @@ def visit(files: Iterable[Path], recursive: bool = False, verbose: bool = False)
             )
 
 
-def compute_filename(
-    file: Path,
-    hfunc: Callable,
-    length: int = 0,
-    prefix: str = None,
-    suffix: str = None,
-    keep_ext: bool = False,
-):
+def compute_hash(hfunc: Callable, file: Path):
     algo = hfunc()
     with file.resolve().open("rb") as fp:
         for chunk in iter(lambda: fp.read(4096), b""):
             algo.update(chunk)
+    return algo.hexdigest()
+
+
+def compute_filename(
+    fingerprint: str,
+    length: int = 0,
+    prefix: str = None,
+    suffix: str = None,
+    extension: str = None,
+):
     return (
         (prefix or "")
-        + (algo.hexdigest()[0:length] if length > 0 else algo.hexdigest())
+        + (fingerprint[0:length] if length > 0 else fingerprint)
         + (suffix or "")
-        + (file.suffix if keep_ext else "")
+        + (extension or "")
     )
 
 
@@ -90,6 +94,13 @@ def main():
         "--dryrun",
         action="store_true",
         help="dry-run mode, do not change anything",
+    )
+    parser.add_argument(
+        "-j",
+        "--jobs",
+        type=int,
+        metavar="THREADS",
+        help="parallel jobs",
     )
     group = parser.add_mutually_exclusive_group()
     for hlabel, hfunc in (
@@ -160,44 +171,52 @@ def main():
     args = parser.parse_args()
 
     count_already_named, count_error, count_renamed = 0, 0, 0
-    for source in visit(args.files, recursive=args.recursive, verbose=args.verbose):
-        newfilename = compute_filename(
-            source,
-            args.hfunc,
-            length=args.length,
-            keep_ext=args.ext,
-            prefix=args.prefix,
-            suffix=args.suffix,
-        )
-        assert len(newfilename) > 0
-        target = (args.folder or source.parent) / newfilename
 
-        if source == target:
-            count_already_named += 1
-            if args.verbose:
-                print(f"{ICON_OK} {label(source)} is already renamed")
-        elif target.exists():
-            count_error += 1
-            print(
-                f"{ICON_EXISTS} {label(source)} cannot be renamed {label(target)}: {Fore.RED}destination already exists{Fore.RESET}"
+    with ThreadPoolExecutor(max_workers=args.jobs) as executor:
+        jobs = {
+            executor.submit(compute_hash, args.hfunc, f): f
+            for f in visit(args.files, recursive=args.recursive, verbose=args.verbose)
+        }
+        for job in as_completed(jobs):
+            source = jobs[job]
+            fingerprint = job.result()
+            newfilename = compute_filename(
+                fingerprint,
+                length=args.length,
+                prefix=args.prefix,
+                suffix=args.suffix,
+                extension=source.suffix if args.ext else None,
             )
-        elif args.dryrun:
-            count_renamed += 1
-            print(
-                f"{ICON_DRYRUN} {label(source)} would be renamed {label(target)} {Fore.CYAN}(dryrun){Style.RESET_ALL}"
-            )
-        else:
-            try:
-                if not target.parent.exists():
-                    target.parent.mkdir(parents=True)
-                shutil.move(source, target)
-                count_renamed += 1
-                print(f"{ICON_OK} {label(source)} was renamed {label(target)}")
-            except BaseException as e:
+
+            assert len(newfilename) > 0
+            target = (args.folder or source.parent) / newfilename
+
+            if source == target:
+                count_already_named += 1
+                if args.verbose:
+                    print(f"{ICON_OK} {label(source)} is already renamed")
+            elif target.exists():
                 count_error += 1
                 print(
-                    f"{ICON_ERROR} {label(source)} cannot be renamed {label(target)}: {Fore.RED}{e}{Fore.RESET}"
+                    f"{ICON_EXISTS} {label(source)} cannot be renamed {label(target)}: {Fore.RED}destination already exists{Fore.RESET}"
                 )
+            elif args.dryrun:
+                count_renamed += 1
+                print(
+                    f"{ICON_DRYRUN} {label(source)} would be renamed {label(target)} {Fore.CYAN}(dryrun){Style.RESET_ALL}"
+                )
+            else:
+                try:
+                    if not target.parent.exists():
+                        target.parent.mkdir(parents=True)
+                    shutil.move(source, target)
+                    count_renamed += 1
+                    print(f"{ICON_OK} {label(source)} was renamed {label(target)}")
+                except BaseException as e:
+                    count_error += 1
+                    print(
+                        f"{ICON_ERROR} {label(source)} cannot be renamed {label(target)}: {Fore.RED}{e}{Fore.RESET}"
+                    )
 
     if count_renamed:
         print(
